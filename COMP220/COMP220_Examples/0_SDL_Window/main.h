d4093747cefd3b1789ca01bdd90f49b6e2e20f42
Stompyy <richvon2208@googlemail.com>
2017-09-30 13:24:48 +0100

    Followed all live coding videos

--- COMP220/COMP220_Examples/0_SDL_Window/main.h
+++ COMP220/COMP220_Examples/0_SDL_Window/main.h
@@ -1,3 +1,8 @@
 #pragma once
 
 #include <SDL.h>
+#include <GL/glew.h>
+#include <SDL_opengl.h>
+#include <iostream>
+//copy "..\..\..\Libraries\SDL2-2.0.6\lib\x64\SDL2.dll" "$(OutDir)\SDL2.dll"
+//copy "..\..\..\Libraries\glew-2.1.0\bin\Release\x64\glew32.dll" "$(OutDir)\glew32.dll"
